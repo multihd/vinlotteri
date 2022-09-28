7138565b2aecfff84f2e807cedde3d12fcfcc461
multihd <elias.helgeland@gmail.com>
2022-09-28 10:26:04 +0200

    Add files via upload

---
+++ vinLotteri.py
@@ -0,0 +1,22 @@
+import random
+print("Ikke la sigurd kjøre, han er QA og kræsjer programmer")
+print("Skriv inn navn og sum per person: eksempel for Sigud så han klarer å kjøre programmet \n\n Martin 100")
+word = ""
+liste = []
+while True:
+    lal = input().split()
+    if(lal[0] == "stop"):
+        break
+    elif (lal.__len__() < 2 or lal.__len__() > 2):
+        continue
+    elif(int(lal[1]) % 25 == 0):
+        lodd = int(int(lal[1])/25)
+        for x in range(lodd):
+            liste.append(lal[0])
+        word = lal[0]
+print("\n")
+random.shuffle(liste)
+print(*liste, sep="\n")
+
+#kjempefin kode :)
+#Created by: Elias Helgeland ;)
